e6e05a9cdcd0fa1598a2a22c582f6f37b2aff615
rordev87 <rordev87@proton.me>
2016-04-25 16:01:54 +0200

    add notification but still not very good and we still creating notifications through console

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
 
-  sockets_for :notifications , only: [:show ]
+  sockets_for :notifications , only: [:show]
   
   sockets_for :users do
     sockets_for :notifications
