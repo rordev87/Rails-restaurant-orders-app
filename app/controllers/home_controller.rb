3f211c39cdd13711aec824fc91bd1c0a3d44c073
rordev87 <rordev87@proton.me>
2016-04-27 18:09:19 +0200

    fixing friends activity without ui or links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,9 +25,13 @@ class HomeController < ApplicationController
 
 
   def getorderbyfriends
-    friends_ids=current_user.followers.map{|u| u.id}; 
-    @orders_of_friends = OrderUserJoin.where(user_id: friends_ids).order(:created_at => :desc).limit(5).map{|m| Order.find(m.order_id)} + Order.where( user_id: friends_ids).order(:created_at => :desc).limit(5).map{ |o| {:admin => User.find(o.user_id) , :order => o } }
-    @orders_of_friends = @orders_of_friends.uniq.sort{|a,b| b.created_at <=> a.created_at }[0..4] 
+    #friends_ids=current_user.follows.map{|u| u.id};
+    friends_ids=current_user.follows.map{|u| u.followable_id}
+    #friends_ids= User.all.map{ |u| u.id} 
+    #@orders_of_friends_joined = OrderUserJoin.where(user_id: friends_ids).order(:created_at => :desc).limit(5).map{ |m| Order.find(:order => m.order_id , :admin => )} 
+    @orders_of_friends = Order.where( user_id: friends_ids).order(:created_at => :desc).limit(5).map{ |o| {:admin => User.find(o.user_id) , :order => o } }
+    #
+    #@orders_of_friends = @orders_of_friends.uniq.sort{|a,b| b.created_at <=> a.created_at }[0..4] 
     
     #.each{|u| u.user_id = User.find(u.user_id) } #.map{ |m| Order.find(m.order_id) };
     #@order_of_friends
