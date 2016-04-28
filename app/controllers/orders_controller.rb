f64f2ca8cdf56db484fcb2b9dd91b9aa5d13b314
rordev87 <rordev87@proton.me>
2016-04-28 16:51:10 +0200

    showed image and create order tested

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -109,8 +109,9 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
 #<<<<<<< HEAD
-    @order = current_user.orders.build(order_params)
-    @order.meal = params[:meal]
+    #@order = current_user.orders.build(order_params)
+    @order = Order.new(order_params)
+    #@order.meal = params[:meal]
     @order.status = "Waiting"
     @order.user_id = current_user.id
     @restaurant = @order.restaurant
@@ -125,13 +126,13 @@ class OrdersController < ApplicationController
 #>>>>>>> d922137f601ec6520049b8246b81626000fc5806
     respond_to do |format|
       if @order.save
-        @orderUser=OrderUserJoin.new
-        @orderUser.order_id=@order.id
-        @orderUser.user_id=User.where(name: params["friend"]).take.id
-        @orderUser.is_joined=0
-        @orderUser.save
-        create_notification("#{current_user.name} has invited you to his order",@orderUser.user_id,current_user.id,@order.id)
-        puts "hellllllllllllllllllllllllllo"+@order.id.to_s+"and again"+User.where(name: params["friend"]).take.id.to_s
+        #@orderUser=OrderUserJoin.new
+        #@orderUser.order_id=@order.id
+        #@orderUser.user_id=User.where(name: params["friend"]).take.id
+        #@orderUser.is_joined=0
+        #@orderUser.save
+        #create_notification("#{current_user.name} has invited you to his order",@orderUser.user_id,current_user.id,@order.id)
+        #puts "hellllllllllllllllllllllllllo"+@order.id.to_s+"and again"+User.where(name: params["friend"]).take.id.to_s
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
