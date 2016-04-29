77ef2a7304756662c06a039c326ea7b7e232b3d9
rordev87 <rordev87@proton.me>
2016-04-29 00:26:47 +0000

    add sqlite to gem file

--- db/migrate/20160427130854_add_order_id_to_notifcation.rb
+++ db/migrate/20160427130854_add_order_id_to_notifcation.rb
@@ -1,5 +1,5 @@
 class AddOrderIdToNotifcation < ActiveRecord::Migration
   def change
-    add_column :notifications, :order_id, :integer , null: false
+    add_column :notifications, :order_id, :integer
   end
 end
